427347c1ab943321af6116c88589fb6c8bc81ec1
𝕂𝕦𝕟𝕕𝕒𝕟 𝕐𝕒𝕕𝕒𝕧 😎 <kundanvirajbhatt@gmail.com>
2025-06-27 18:23:53 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,18 +12,17 @@ from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
 from TechVJ.util.human_readable import humanbytes
 
 async def encode(string):
-    string_bytes = string.encode("ascii")
-    base64_bytes = base64.urlsafe_b64encode(string_bytes)
-    base64_string = base64_bytes.decode("ascii").rstrip("=")  # Remove trailing padding only
-    return base64_string
+    string_bytes = string.encode("ascii")
+    base64_bytes = base64.urlsafe_b64encode(string_bytes)
+    base64_string = (base64_bytes.decode("ascii")).strip("=")
+    return base64_string
 
 async def decode(base64_string):
-    # Add correct padding back
-    base64_string += "=" * (-len(base64_string) % 4)
-    base64_bytes = base64_string.encode("ascii")
-    string_bytes = base64.urlsafe_b64decode(base64_bytes)
-    string = string_bytes.decode("ascii")
-    return string
+    base64_string = base64_string.strip("=") # links generated before this commit will be having = sign, hence striping them to handle padding errors.
+    base64_bytes = (base64_string + "=" * (-len(base64_string) % 4)).encode("ascii")
+    string_bytes = base64.urlsafe_b64decode(base64_bytes) 
+    string = string_bytes.decode("ascii")
+    return string
 
 @Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
